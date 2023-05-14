fc6a8b2bc1d1a31b2644f0c7e6b6ebe6234708f0
DanielIgnacioCarcamo <carcamodaniel.informatica@gmail.com>
2023-05-14 18:29:11 -0400

    arreglos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,4 @@
 const spanNode = document.getElementsByTagName("span");
-
-/* funcion para mostrar total */
 function mostrar() {
   const cantidad = +document.getElementsByTagName("input")[0].value;
   spanNode[0].innerHTML = "$" + 60000 * cantidad;
