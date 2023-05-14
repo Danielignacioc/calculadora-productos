266ac8ed9dd5e1a3a6f05b700be6e4dd7d9632d4
DanielIgnacioCarcamo <carcamodaniel.informatica@gmail.com>
2023-05-14 18:25:57 -0400

    arreglos

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,8 @@ const spanNode = document.getElementsByTagName("span");
 function mostrar() {
   const cantidad = +document.getElementsByTagName("input")[0].value;
   spanNode[0].innerHTML = "$" + 60000 * cantidad;
-
+  spanNode[1].innerHTML = cantidad; 
+ 
   const color = document.querySelector("#color");
   let paletaColor = document.querySelector("#paleta-color");
 
